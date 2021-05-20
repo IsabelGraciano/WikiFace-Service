aa8d0b3f809a2656b78d1402e776f60c481d3101
Camila Barona <cbaronac@eafit.edu.co>
2021-05-20 13:17:17 -0500

    first view

--- MydjangoProject/settings.py
+++ MydjangoProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'wikiface.apps.WikifaceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
